f55aaba9bb2f61ced1843bf7c7b7fc73692ebd25
Jacopo Martelossi <62595944+jacopoM28@users.noreply.github.com>
2023-03-01 11:33:01 +0100

    Update 05.LINEs_Identification.sh

--- 05.LINEs_Identification.sh
+++ 05.LINEs_Identification.sh
@@ -21,7 +21,7 @@ for i in $(ls | grep "_TransposableElements"); do
 #-------------------------------Extract all  LINEs autonomous copies and confirm LINE RVT domain through hmmscan---------------------#
   #RVT_MolluscaP.hmm is a simple database composed by two HMM profiles builded up from LTRs and LINEs-derived RVT
   awk '{ if ((NR>1)&&($0~/^>/)) { printf("\n%s", $0); } else if (NR==1) { printf("%s", $0); } else { printf("\t%s", $0); } }' "$varSpecie"_ExtractedSequences_ORF.pep | grep -Ff LINEs/"$varSpecie"_LINES.txt - | tr "\t" "\n" > LINEs/"$varSpecie"_Putative_LINES_RVT.ORF.fasta
-  hmmscan -E 1e-05 --cpu 8  --domtblout LINEs/"$varSpecie"_Putative_LINES_RVT.ORF.domtblout --tblout LINEs/"$varSpecie"_Putative_LINES_RVT.ORF.tblout ../../../../data/RVT_MolluscaP.hmm LINEs/"$varSpecie"_Putative_LINES_RVT.ORF.fasta
+  hmmscan -E 1e-05 --cpu 8  --domtblout LINEs/"$varSpecie"_Putative_LINES_RVT.ORF.domtblout --tblout LINEs/"$varSpecie"_Putative_LINES_RVT.ORF.tblout ../../../../data/RVT_MolluscaPfam.hmm LINEs/"$varSpecie"_Putative_LINES_RVT.ORF.fasta
   #Print bet-hitted domain for each query sequences
   awk '!x[$4]++' LINEs/"$varSpecie"_Putative_LINES_RVT.ORF.domtblout > LINEs/"$varSpecie"_Putative_LINES_RVT.ORF.domtblout_BestHits
   cat LINEs/"$varSpecie"_Putative_LINES_RVT.ORF.domtblout_BestHits | grep "LINE"  | grep -v "#" | awk '{print$4}' > LINEs/"$varSpecie"_LINEs_RVT.ORF_Confirmed.txt
